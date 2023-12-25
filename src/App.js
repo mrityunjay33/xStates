3103322df385dd82299fa007ba3ae70c32bcb8d6
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-25 21:25:48 +0530

    Feat: State, country, city

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [stateList, setStateList] = useState([]);
+  const [countryList, setCountryList] = useState([]);
+  const [cityList, setCityList] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedState, setSelectedState] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+  const [userText, setUserText] = useState('');
+  
+
+
+  async function fetchData(url) {
+    try{
+      const response = await fetch(url);
+      const data =   await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    fetchData('https://crio-location-selector.onrender.com/countries').then((data) => {
+      // console.log(data);
+      setCountryList(data);
+    })
+    
+  },[]);
+
+  const handleCountry = (e) => {
+    setSelectedCountry(e.target.value);
+    fetchData(`https://crio-location-selector.onrender.com/country=${e.target.value}/states`).then((data) => {
+        setStateList(data);
+        setCityList([]);
+    })
+  }
+  const handleState = (e) => {
+    setSelectedState(e.target.value);
+    fetchData(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${e.target.value}/cities`).then((data) => {
+        setCityList(data);
+    })
+  }
+  const handleCity = (e) => {
+    setSelectedCity(e.target.value);
+    setUserText(`You Seleced ${e.target.value}, ${selectedState}, ${selectedCountry}`);
+  }
 
   return (
     <div className='container'>
-      <h1>XStates</h1>
+      <h1>Select Location</h1>
+      <form>
+        <select name='country' onChange={handleCountry} >
+          <option value='Select Country' selected >Select Country</option>
+          {countryList.map((country, idx)=><option key={idx} value={country} >{country}</option>)}
+        </select>
+        <select name='states' onChange={handleState} >
+          <option value='Select State' selected >Select State</option>
+          {stateList.map((state, idx)=><option key={idx} value={state} >{state}</option>)}
+        </select>
+        <select name='cities' onChange={handleCity} >
+          <option value='Select City' selected >Select City</option>
+          {cityList.map((city, idx)=><option key={idx} value={city} >{city}</option>)}
+        </select>
+      </form>
+      <div>{userText}</div>
     </div>
   );
 }
